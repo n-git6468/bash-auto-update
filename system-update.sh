ff91695ea9a22452d1d8a9cd8e50f4011d46cdb5
noah <noah@localhost.localdomain>
2020-02-07 13:48:00 -0500

    Logging updates. Other minor updates.

--- system-update.sh
+++ system-update.sh
@@ -5,26 +5,26 @@ PATH=/usr/sbin:/usr/bin:/sbin:/bin
 #Synopsis: Execute dnf check-update, capture exit code, based upon exit code, journal success, journal failure, journal success & update.
 
 auto-update() {
-	#Check for updates, capture exit code.
-	#EXITCODE = 0 --> Successful execution, no updates available
-	#EXITCODE = 1 --> Unsuccessful execution
-	#EXITCODE = 100 --> Successful execution, updates available
-	dnf check-update
-	local EXITCODE=$?
+	  #Check for updates, capture exit code.
+	  #EXITCODE = 0 --> Successful execution, no updates available
+	  #EXITCODE = 1 --> Unsuccessful execution
+	  #EXITCODE = 100 --> Successful execution, updates available
+	  dnf check-update
+	  local EXITCODE=$?
 
-	#Used for testing
-	echo $EXITCODE
+	  #Used for testing
+	  echo $EXITCODE
 
-	#TODO add basic journalctl interop success & unknown failure conditions
-	#TODO add advanced journalctl interop for dnf update -y. Present actions taken on packages within log in a pleasing & digestible manner.
-	if [[ $EXITCODE = 0 ]]
-		then echo "Daily update status: success, no updates available." >> systemd-cat
-	elif [[ $EXITCODE = 1 ]]
-		then echo "Uknown failure, exit code 1."
-	elif [[ $EXITCODE = 100 ]]
-		#TODO add dnf update -y
-		then echo "dnf update -y"
-	fi
+	  #TODO add advanced journalctl interop for dnf update -y. Present actions taken on packages within log in a pleasing & digestible manner.
+    #TODO add service ID to logging
+	  if [[ $EXITCODE = 0 ]]
+    then echo "Daily package update status: success, no updates available." | systemd-cat
+	  elif [[ $EXITCODE = 1 ]]
+    then echo "Daily package update status: unknown failure, exit code 1." | systemd-cat
+	  elif [[ $EXITCODE = 100 ]]
+    then dnf update -y
+    fi
 }
 
+#Call function
 auto-update
