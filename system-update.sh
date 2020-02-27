11fb09b921e507b6e83fbf97bb5b67b070378ff6
noah <noah@localhost.localdomain>
2020-02-26 19:17:47 -0500

    Logging updates. Removed debugging.

--- system-update.sh
+++ system-update.sh
@@ -1,28 +1,29 @@
 #Specify path
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
-
-#A simple function
 #Synopsis: Execute dnf check-update, capture exit code, based upon exit code, journal success, journal failure, journal success & update.
+#TODO log dnf upgrade as one single journalctl entry, currently every dnf STDOUT line is a single journalctl entry.
 
 auto-update() {
-	  #Check for updates, capture exit code.
 	  #EXITCODE = 0 --> Successful execution, no updates available
 	  #EXITCODE = 1 --> Unsuccessful execution
 	  #EXITCODE = 100 --> Successful execution, updates available
+    #See https://dnf.readthedocs.io/en/latest/command_ref.html for additional dnf exit codes
+    #Valid log levels -- info, warning, emerg
+
+    #dnf checks for updates
 	  dnf check-update
+    #exit code is captured
 	  local EXITCODE=$?
 
-	  #Used for testing
-	  echo $EXITCODE
-
-	  #TODO add advanced journalctl interop for dnf update -y. Present actions taken on packages within log in a pleasing & digestible manner.
-    #TODO add service ID to logging
+    #If no packages are available for update, log "...no updates available." to journalctl as 'dnf-cron-update' level info.
 	  if [[ $EXITCODE = 0 ]]
-    then echo "Daily package update status: success, no updates available." | systemd-cat
+    then echo "Daily package update status: success, no updates available." | systemd-cat -t dnf-cron-update -p info
+    #If dnf update check fails, log failure to journalctl as 'dnf-cron-update' level warning
 	  elif [[ $EXITCODE = 1 ]]
-    then echo "Daily package update status: unknown failure, exit code 1." | systemd-cat
+    then echo "Daily package update status: unknown failure, exit code 1." | systemd-cat -t dnf-cron-update -p warning
+    #If package updates are available, log actions to journalctl as 'dnf-cron-update' level info
 	  elif [[ $EXITCODE = 100 ]]
-    then dnf update -y
+    then dnf update -y | systemd-cat -t dnf-cron-update -p info
     fi
 }
 
